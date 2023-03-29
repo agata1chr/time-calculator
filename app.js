aa9d3ad9d4a737b68d540832aa425e1e98192d35
Ewa Niedbała <eniedbala20@gmail.com>
2023-03-29 18:52:09 +0200

    usunięcie ::aftera i dodanie showTime w js

--- app.js
+++ app.js
@@ -9,7 +9,22 @@ const duplicate = document.querySelector('#duplicate');
 const resultbtn = document.querySelectorAll('#resultbtn');
 const prevOperation = document.querySelector('.prevOperation');
 const mathResult = document.querySelector('.mathResult');
-
+let hour;
+let minutes;
 let actualyAction = '';
 let prevAction = '';
 let operation = undefined;
+
+const showTime = (hour, minut) => {
+	if (minut >= 60) {
+		minuty = parseInt(minut / 60);
+		console.log(minuty);
+		hour = hour + minuty;
+		minut = minut - minuty * 60;
+	}
+	if (minut < 0) {
+		console.log('godzina2');
+	}
+	console.log(hour + ' ' + minut);
+};
+showTime(12, 285);
